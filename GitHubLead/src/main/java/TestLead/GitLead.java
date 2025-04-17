040024eb4a7f46e915c11d2c510398538b9b1141
ARUN KUMAR <ARUN KUMAR@LAPTOP-5HV40H5E>
2025-04-17 10:55:58 +0530

    test

---
+++ GitHubLead/src/main/java/TestLead/GitLead.java
@@ -0,0 +1,13 @@
+package TestLead;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitLead {
+	@Test
+	public void github() {
+		Reporter.log("test lead");
+	}
+	
+
+}
